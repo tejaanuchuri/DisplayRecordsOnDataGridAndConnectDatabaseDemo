38ae331606720d99594b7f5bec4a35852e440bc6
tejaanuchuri0009@gmail.com <tejaanuchuri0009@gmail.com>
2021-09-17 01:05:13 +0530

    Add project files.

---
+++ ConnectDatabaseDemo/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConnectDatabaseDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadEmployeeRecordsOnGrid();
+        }
+
+        private void LoadEmployeeRecordsOnGrid()
+        {
+            OleDbConnection connection = new OleDbConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["Connection"].ToString();
+            connection.Open();
+            OleDbCommand command = new OleDbCommand();
+            command.CommandText = "Select * from EmployeeTable";
+            command.Connection = connection;
+            OleDbDataReader rd = command.ExecuteReader();
+            datagrid.ItemsSource = rd;
+        }
+    }
+}
